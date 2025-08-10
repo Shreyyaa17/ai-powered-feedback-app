3966354598de592a8c38b7a0215d83056db1a08b
Shreyyaa17 <shreyaag402@gmail.com>
2025-08-11 00:26:47 +0530

    final touches

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FeedbackForm from './components/FeedbackForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <FeedbackForm>
     <App />
+    </FeedbackForm>
   </React.StrictMode>
 );
 
