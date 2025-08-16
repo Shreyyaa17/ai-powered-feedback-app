8b89d93216f2b110d8796d328f9c5e26886e981f
Shreyyaa17 <shreyaag402@gmail.com>
2025-08-16 15:25:56 +0530

    final

--- server/server.js
+++ server/server.js
@@ -6,21 +6,25 @@ require("dotenv").config();
 const feedbackRoutes = require("./routes/feedbackRoutes");
 
 const app = express();
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000"
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 
 //Routes
-app.use('/api', feedbackRoutes);
+app.use("/api", feedbackRoutes);
 app.get("/", (req, res) => {
   res.send("Server is live");
 });
 
-
 //MongoDB connection
-mongoose.connect(process.env.MONGO_URI || "mongodb+srv://shreyagupta:shreyagupta008@cluster0.tyfdoqm.mongodb.net").then(()=>console.log("MongoDB Connected")).catch(error=>console.log(error));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.log(error));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
